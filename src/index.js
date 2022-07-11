40b9e7258bdce455df1bfab130ab0ee56704b7e2
Ashim Singh <ashimsingh948@gmail.com>
2022-07-11 22:12:29 +0545

    props

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import React from 'react'
+import ReactDom from 'react-dom'
+const bookList= [
+  {
+    img:"https://m.media-amazon.com/images/P/194563183X.01._SCLZZZZZZZ_SX500_.jpg",
+    title:"Things we never get over",
+    author:"Lucy Score",
+  },
+  {
+    img:'https://images-na.ssl-images-amazon.com/images/I/51PRQuO-xjL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg',
+ title:'Our Class isaFamily',
+ author:'Shannon Olsen',
+  }
+]
+function Myapp(){
+  return(
+    bookList.map((books)=>{
+      return(
+        <Book props={books}></Book>
+      )
+    })
+  )
+}
+function Book(props){
+  const {img,title,author}=props.props
+  return(
+    <section>
+      <img src={img}></img>
+    <h2>{title}</h2>
+    <h4>{author}</h4>
+    </section>
+  )
+}
+ReactDom.render(<Myapp/>,document.getElementById('root'))
