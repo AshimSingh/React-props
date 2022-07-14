bdce8eef17c3fef8db207aa70452edf8a22413a9
Ashim Singh <ashimsingh948@gmail.com>
2022-07-14 20:58:53 +0545

    key in prop

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import './index.css'
 const bookList= [
   {
+    id: 1,
     img:"https://m.media-amazon.com/images/P/194563183X.01._SCLZZZZZZZ_SX500_.jpg",
     title:"Things we never get over",
     author:"Lucy Score",
   },
   {
+    id: 2,
     img:'https://images-na.ssl-images-amazon.com/images/I/51PRQuO-xjL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg',
  title:'Our Class isaFamily',
  author:'Shannon Olsen',
@@ -14,21 +17,25 @@ const bookList= [
 ]
 function Myapp(){
   return(
-    bookList.map((books)=>{
-      return(
-        <Book props={books}></Book>
-      )
-    })
+    <section className='Libary'>
+      {
+        bookList.map((puskat)=>{
+          return(
+            <Book key={puskat.id} book={puskat}></Book>
+          )
+        })
+      }
+    </section>
   )
 }
 function Book(props){
-  const {img,title,author}=props.props
+  const {img,title,author}=props.book
   return(
-    <section>
-      <img src={img}></img>
+    <div className='books'>
+      <img src={img} alt="hello"></img>
     <h2>{title}</h2>
     <h4>{author}</h4>
-    </section>
+    </div>
   )
 }
 ReactDom.render(<Myapp/>,document.getElementById('root'))
