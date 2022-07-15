7f510cc6e014298c95fc47765a33d6720d83808b
Ashim Singh <ashimsingh948@gmail.com>
2022-07-15 18:42:30 +0545

    Event basic

--- src/index.js
+++ src/index.js
@@ -30,11 +30,22 @@ function Myapp(){
 }
 function Book(props){
   const {img,title,author}=props.book
+  const myfun =(e)=>{
+    console.log(e)
+    console.log(e.target)
+    alert("Hello ashim")
+  }
+  const ref=(author)=>{
+    
+    console.log(author)
+  }
   return(
     <div className='books'>
-      <img src={img} alt="hello"></img>
-    <h2>{title}</h2>
+      <img onMouseOver={()=>console.log(title)} src={img} alt="hello"></img>
+    <h2 onClick={()=>console.log("you are reading "+ title)}>{title}</h2>
     <h4>{author}</h4>
+    <button onClick={myfun}>Alert</button>
+    <button onClick={()=>ref(author)}>Refrence</button>
     </div>
   )
 }
