88bcdb204b11362af5e8682591a3232ba6a06533
Ashim Singh <ashimsingh948@gmail.com>
2022-07-25 21:33:54 +0545

    array filter

--- src/tutorials/Heading.js
+++ src/tutorials/Heading.js
@@ -3,12 +3,42 @@ import { data } from './data'
 import { useState} from 'react'
 
 const UseState=()=>{
+    let delt =true
     const [person,setPerson] =useState(data)
+    const remove=(id)=>{
+        let removePeople = person.filter((p)=>p.id !== id)
+        setPerson(removePeople)
+    }
     return(
-        <div className='Heading'>
-            <h1>Heading</h1>
-            person.map()
-        </div>
+        <>
+            {
+                person.map((people)=>{
+                    const {id,name}=people
+                    return(
+                        <div key={id} className='item'>
+                            <h1>{name}</h1>
+                            <button onClick={()=>remove(id)}>Remove</button>
+                        </div>
+                    )
+                })
+            }
+            <button className='btn' onClick={del}>Delete Name</button>
+        </>
     )
+    
+    function del(){
+        
+        const btn = document.querySelector(".btn")
+        if(delt===true){
+            btn.innerHTML='add Item'
+            setPerson([])
+            delt=false
+        }
+        else{
+            
+            btn.innerHTML="Delete Items"
+        }
+    }
 }
+
 export default UseState
