4d3d8433a9609584260df8921990e0395c16c7a0
Ashim Singh <ashimsingh948@gmail.com>
2022-07-28 09:57:28 +0545

    sloved decrease problem

--- src/tutorials/SimpleCounter.js
+++ src/tutorials/SimpleCounter.js
@@ -3,13 +3,7 @@ import {useState} from 'react'
 
 const Counter= ()=>{
     const [value,setvalue]=useState(0)
-    const decrease=(value)=>{
-        alert({value})
-        // if({value}>0)
-        // {
-        //     setvalue(value-1)
-        // }
-    }
+    
     return(
         <>
             <section style={{margin:'4rem 40%'}}>
@@ -19,7 +13,7 @@ const Counter= ()=>{
             <div style={{margin:'4rem 36%'}}>
                 <button className="btn" onClick={()=>{setvalue(value+1)}}>Increase</button>
                 <button className="btn" onClick={()=>{setvalue(0)}}>Reset</button>
-                <button className="btn" onClick={()=>decrease(value)}>Decrease</button>
+                <button className="btn" onClick={()=>{if(value>0){setvalue(value-1)}}}>Decrease</button>
             </div>
         </>
     )
