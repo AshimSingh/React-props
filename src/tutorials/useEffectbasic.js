5aa1ef0af7c26ea5816b88af4bd635314249f81c
Ashim Singh <ashimsingh948@gmail.com>
2022-08-03 14:11:48 +0545

    clean up function useEffect

--- src/tutorials/useEffectbasic.js
+++ src/tutorials/useEffectbasic.js
@@ -4,19 +4,36 @@ import { useState } from 'react'
 
 const UseEffectBasic=()=>{
     const [value,setvalue]=useState(0)
+    const[size,resize]=useState(window.innerWidth)
     useEffect(()=>{
         console.log("use effect")
         if(value>0){
             document.title=`(${value}) New Message`
         }
-    })   
-    console.log("console")
+        // const btn = document.querySelector(".btn")
+        // btn.addEventListener("click",()=>{
+        //     console.log("Hello ashim")
+        // })
+    },[value])
+    const checkSize=()=>{
+        resize(window.innerWidth)
+    }
+    useEffect(()=>{
+        console.log("use Effect*2")
+        window.addEventListener('resize',checkSize)
+        // return(()=>{
+        //     console.log("clean Up")
+        //     window.removeEventListener('resize',checkSize)
+        // })
+    },[])
     return(
         <>
             <h1>
                 {value}
             </h1>
             <h3>Good Morning Ashim, Greetings!!!</h3>
+            <h1>Window Size</h1>
+            <h3>{size}</h3>
             {/* <h2>{data}</h2> */}
             <button className='btn' onClick={()=>setvalue(value+1)}>Button</button>
         </>
