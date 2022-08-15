ffdb96f548aeb6fbd509c99706aa8122e33e22d5
Ashim Singh <ashimsingh948@gmail.com>
2022-08-15 09:23:42 +0545

    fetch as in yt

--- src/tutorials/fetchdata.js
+++ src/tutorials/fetchdata.js
@@ -18,23 +18,33 @@ const Fetchdata= ()=>{
     return(
         <>
             <div className="container"><h1>Fectch Data</h1></div>
+            <ul className="users">
             {
                 user.map((data)=>{
                     const {id,login,avatar_url,html_url}=data
                     return(
                         <>
-                            <div className='tiles' key={id}>
+                            {/* <div className='tiles' key={id}>
                             <img className='bImg' src={avatar_url}></img>
                             <div className='title'>
                                 <div className='suffix'>
                                 {login}
                                 </div>
                             </div>
-                            </div>
+                            </div> */}
+                            <li key={id}>
+                                <img src={avatar_url} alt={login}></img>
+                                <div>
+                                    <h4>{login}</h4>
+                                    <a href={html_url}>Profile</a>
+                                </div>
+
+                            </li>
                         </>
                     )
                 })
             }
+            </ul>
         </>
     )
 }
